b1d060ce7a3edf5302a35b176b7edc66d754627b
mrdonuzz <zsqxaw123@gmail.com>
2021-04-26 16:31:53 +0700

    lab4

---
+++ lap4.component.ts
@@ -0,0 +1,13 @@
+import { Component, Input} from '@angular/core';
+
+@Component({
+  selector: 'app-lap4',
+  templateUrl: './lap4.component.html',
+  styleUrls: ['./lap4.component.css']
+})
+
+export class Lap4Component {
+  objects = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  constructor() {}
+}
